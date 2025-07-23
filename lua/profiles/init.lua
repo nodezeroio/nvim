a656e27492b128f8f7a9ae9b9c462e793a198892
Thomas Bell <thomas@thomasbell.io>
2025-07-23 14:48:55 -0600

    moved core profile into a directory

--- lua/profiles/init.lua
+++ lua/profiles/init.lua
@@ -8,7 +8,7 @@ local profiles = {
 function M.setup()
   local plugin_manager = require("utils.plugin-manager")
   for _, name in ipairs(profiles) do
-    local plugin = require("profiles." .. name)
+    local plugin = require("profiles." .. name .. ".plugins")
     -- Use the enhanced ensure_and_setup function
     plugin_manager.ensure_and_setup(plugin)
   end
