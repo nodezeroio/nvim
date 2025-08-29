97410221ae9ad2959ceaba291f956a99182480d7
Thomas Bell <thomas@thomasbell.io>
2025-08-29 13:00:32 -0600

    added an ansible profile

---
+++ lua/nodezero/profiles/ansible.lua
@@ -0,0 +1,34 @@
+return {
+    plugins = {
+        {
+            "nvim-treesitter/nvim-treesitter",
+            url = "git@github.com:nodezeroio/nvim-treesitter.git",
+            opts = {
+                ensure_installed = {
+                    "yaml",
+                    "jinja",
+                    "jinja_inline"
+                },
+            },
+        },
+        {
+            "stevearc/conform.nvim",
+            url = "git@github.com:nodezeroio/conform.nvim.git",
+            opts = {
+                formatters_by_ft = {
+                    yaml = function(bufnr)
+                        local path = vim.api.nvim_buf_get_name(bufnr)
+                        if path:match("/playbooks/") or
+                            path:match("/roles/") or
+                            path:match("/group_vars/") or
+                            path:match("/host_vars/") or
+                            path:match("%.ansible%.ya?ml$") then
+                            return { "ansible-lint", "yamlfmt" }
+                        end
+                        return { "yamlfmt" }
+                    end,
+                },
+            },
+        },
+    },
+}
