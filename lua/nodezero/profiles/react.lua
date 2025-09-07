34f3899552214acecd29c592a499d2f440d4d78a
Thomas Bell <thomas@thomasbell.io>
2025-09-07 11:28:26 -0600

    added the react profile

---
+++ lua/nodezero/profiles/react.lua
@@ -0,0 +1,34 @@
+return {
+  plugins = {
+    {
+      "nvim-treesitter/nvim-treesitter",
+      url = "git@github.com:nodezeroio/nvim-treesitter.git",
+      opts = {
+        ensure_installed = {
+          "javascript",
+          "typescript",
+        },
+      },
+    },
+    {
+      "mfussenegger/nvim-lint",
+      url = "git@github.com:nodezeroio/nvim-lint.git",
+      opts = {
+        linters_by_ft = {
+          ts = { "biome" },
+          js = { "biome" },
+        },
+      },
+    },
+    {
+      "stevearc/conform.nvim",
+      url = "git@github.com:nodezeroio/conform.nvim.git",
+      opts = {
+        formatters_by_ft = {
+          js = { "biome" },
+          ts = { "biome" },
+        },
+      },
+    },
+  },
+}
